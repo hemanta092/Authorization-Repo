e1f525cc3b99ebb0e61274ea36f78aa562458e7d
Hemanta Sharma <hmnt.sharma092@gmail.com>
2022-06-18 19:16:30 +0530

    cross-origin added

--- src/main/java/com/api/authorization/controller/AuthorizationController.java
+++ src/main/java/com/api/authorization/controller/AuthorizationController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -31,6 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/auth")
 @Slf4j
+@CrossOrigin
 public class AuthorizationController {
 	
 	@Autowired
